53d89793b92b44f1729571bd27afcc53cc05a5e7
Luong NGUYEN <141319935+Luonguyez@users.noreply.github.com>
2023-08-30 16:26:52 +0700

    Update BT_Cap_words.c
     update “Autosar C Coding Guidelines”

--- BaitapEmbeded/BT_Cap_words.c
+++ BaitapEmbeded/BT_Cap_words.c
@@ -1,8 +1,23 @@
+/*
+* File: BT_Cap_words.c
+* Author: Luong Nguyen
+* Date: 28/08/2023
+* Description:
+     1. Capitalize the letters after the dot (.).
+*/
+//header 
 #include <stdio.h>
 #include<string.h>
 
 char string[] ="mot hai ba. bon nam sau bay";
-
+/*
+* Function: 
+* Description: This function in order to capitalize the letters after the dot.
+* Input:
+*   char string[]
+* Output:
+*   printf the string after capitalize
+*/
 void Capwords(char string[]){
     
     for(int i = 0; string[i]!='\0'; i++)
